bb81c7b7edb24de72eac915cbc05eeb657c9f4b3
kakkydon <45267235+kakkydon@users.noreply.github.com>
2020-05-30 02:29:00 +0900

    ee

--- src/components/topPage/topMain.tsx
+++ src/components/topPage/topMain.tsx
@@ -1,5 +1,9 @@
 import React, { FC } from 'react';
 import {createStyles, makeStyles} from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import SearchIcon from '@material-ui/icons/Search';
+import IconButton from '@material-ui/core/IconButton';
+import InputBase from '@material-ui/core/InputBase';
 
 import flower from '../../assets/images/flower.jpg';
 
@@ -11,6 +15,14 @@ const useStyle = makeStyles(() =>
             backgroundImage: `url(${flower})`,// typeScriptのコードを文字列に埋め込む際は$をつける　かつ　`で囲む
             backgroundSize: 'cover',
         },
+
+        paper: {
+            position: 'relative',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            top:'33%',
+            width:'45%',
+        }
     }),
 )
 
@@ -18,6 +30,14 @@ const TopMain: FC = () =>{
     const classes = useStyle();
     return(
         <div className={classes.background}>
+            <Paper className={classes.paper}>
+                <IconButton type='submit'>
+                    <SearchIcon />
+                </IconButton>
+                <InputBase 
+                placeholder='無料素材を検索'
+                />
+            </Paper>
 
         </div>
     )
